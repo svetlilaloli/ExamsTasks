fc5190e56661f890deac77334f4aef5f4a25cb5e
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-05 17:15:26 +0100

    Initial commit

---
+++ LootBox/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LootBox
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var firstLootBox = Array.ConvertAll(Console.ReadLine().Split(), int.Parse).ToList();
+            var secondLootBox = Array.ConvertAll(Console.ReadLine().Split(), int.Parse).ToList();
+            List<int> claimedItems = new List<int>();
+
+            while (firstLootBox.Count > 0 && secondLootBox.Count > 0)
+            {
+                int first = firstLootBox[0];
+                int second = secondLootBox[secondLootBox.Count - 1];
+                int sum = first + second;
+                
+                if (sum % 2 == 0)
+                {
+                    claimedItems.Add(sum);
+                    firstLootBox.RemoveAt(0);
+                    secondLootBox.RemoveAt(secondLootBox.Count - 1);
+                }
+                else
+                {
+                    firstLootBox.Add(second);
+                    secondLootBox.RemoveAt(secondLootBox.Count - 1);
+                }
+            }
+            if (firstLootBox.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            int itemsSum = claimedItems.Sum();
+            
+            if (itemsSum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {itemsSum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {itemsSum}");
+            }
+        }
+    }
+}
---
+++ ReVolt/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+
+namespace ReVolt
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int commandsCount = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+            int i = -1, j = -1;
+            bool playerWon = false;
+
+            for (int row = 0; row < n; row++)
+            {
+                matrix[row] = Console.ReadLine().ToCharArray();
+                if (matrix[row].Contains('f'))
+                {
+                    i = row;
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (matrix[row][col] == 'f')
+                        {
+                            j = col;
+                        }
+                    }
+                }
+            }
+
+
+            for (int c = 0; c < commandsCount; c++)
+            {
+                string command = Console.ReadLine();
+                
+                matrix[i][j] = '-';
+                
+                switch (command)
+                {
+                    case "up":
+                        {
+                            MoveBack(ref i, n);
+
+                            if (matrix[i][j] == 'T')
+                            {
+                                MoveForward(ref i, n);
+                            }
+                            else if (matrix[i][j] == 'B')
+                            {
+                                MoveBack(ref i, n);
+                            }
+                        }
+                        break;
+                    case "down":
+                        {
+                            MoveForward(ref i, n);
+                            if (matrix[i][j] == 'T')
+                            {
+                                MoveBack(ref i, n);
+                            }
+                            else if (matrix[i][j] == 'B')
+                            {
+                                MoveForward(ref i, n);
+                            }
+                        }
+                        break;
+                    case "left":
+                        {
+                            MoveBack(ref j, n);
+                            if (matrix[i][j] == 'T')
+                            {
+                                MoveForward(ref j, n);
+                            }
+                            else if (matrix[i][j] == 'B')
+                            {
+                                MoveBack(ref j, n);
+                            }
+                        }
+                        break;
+                    case "right":
+                        {
+                            MoveForward(ref j, n);
+                            if (matrix[i][j] == 'T')
+                            {
+                                MoveBack(ref j, n);
+                            }
+                            else if (matrix[i][j] == 'B')
+                            {
+                                MoveForward(ref j, n);
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (matrix[i][j] == 'F')
+                {
+                    playerWon = true;
+                    matrix[i][j] = 'f';
+                    break;
+                }
+                
+                matrix[i][j] = 'f';
+            }
+
+            if (playerWon)
+            {
+                Console.WriteLine("Player won!");
+            }
+            else
+            {
+                Console.WriteLine("Player lost!");
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(char[][] matrix)
+        {
+            foreach (char[] row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+        private static void MoveBack(ref int i, int n)
+        {
+            if (i == 0)
+            {
+                i = n - 1;
+            }
+            else
+            {
+                i--;
+            }
+        }
+        private static void MoveForward(ref int i, int n)
+        {
+            if (i == n - 1)
+            {
+                i = 0;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+}

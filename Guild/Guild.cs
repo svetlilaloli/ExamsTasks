445e9093084f1e98236ca46d74c392d9034fa2a8
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-06 07:12:28 +0100

    Guild project added

---
+++ Guild/Guild.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private readonly List<Player> roster;
+        public string Name { get; private set; }
+        public int Capacity { get { return roster.Capacity; } private set { } }
+        public int Count => roster.Count;
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            roster = new List<Player>(capacity);
+        }
+
+        public void AddPlayer(Player player)
+        {
+            roster.Add(player);
+        }
+        public bool RemovePlayer(string name)
+        {
+            return roster.Remove(roster.Find(x => x.Name == name));
+        }
+        public void PromotePlayer(string name)
+        {
+            roster.Find(p => p.Name == name).Rank = "Member";
+        }
+        public void DemotePlayer(string name)
+        {
+            roster.Find(p => p.Name == name).Rank = "Trial";
+        }
+        public Player[] KickPlayersByClass(string pClass)
+        {
+            Player[] kicked = roster.Where(p => p.Class == pClass).ToArray();
+            
+            roster.RemoveAll(p => p.Class == pClass);
+            
+            return kicked;
+        }
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append($"Players in the guild: {Name}\n");
+
+            foreach (Player player in roster)
+            {
+                result.Append($"{player}\n");
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Guild/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace Guild
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public string Class { get; private set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+
+        public Player(string name, string pClass)
+        {
+            Name = name;
+            Class = pClass;
+            Rank = "Trial";
+            Description = "n/a";
+        }
+        public override string ToString()
+        {
+            return $"Player { Name}: { Class}\nRank: { Rank}\nDescription: { Description}";
+        }
+    }
+}

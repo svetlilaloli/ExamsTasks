da79ecf5f087ca4aa39b8f1189dd861892828fd6
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-10 16:42:55 +0100

    Scheduling task added

---
+++ Scheduling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> tasks = Array.ConvertAll(Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse).ToList();
+            List<int> threads = Array.ConvertAll(Console.ReadLine().Split(), int.Parse).ToList();
+            int taskToBeKilled = int.Parse(Console.ReadLine());
+
+            while (taskToBeKilled != tasks[tasks.Count - 1])
+            {
+                if(threads[0] >= tasks[tasks.Count - 1])
+                {
+                    tasks.RemoveAt(tasks.Count - 1);
+                }
+                threads.RemoveAt(0);
+            }
+            Console.WriteLine($"Thread with value {threads[0]} killed task {taskToBeKilled}");
+            Console.WriteLine(string.Join(" ", threads));
+        }
+    }
+}

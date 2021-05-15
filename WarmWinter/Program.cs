8bb461196d7ba262916c1e02c5375ae7dea3fd64
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-05-15 22:22:45 +0100

    Spelling error fixed in WarmWinter task

--- WarmWinter/Program.cs
+++ WarmWinter/Program.cs
@@ -9,25 +9,25 @@ namespace WarmWinter
         public static void Main()
         {
             List<int> hats = Array.ConvertAll(Console.ReadLine().Split(), int.Parse).ToList();
-            List<int> scarfs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse).ToList();
+            List<int> scarves = Array.ConvertAll(Console.ReadLine().Split(), int.Parse).ToList();
             List<int> sets = new List<int>();
 
-            while (hats.Count > 0 && scarfs.Count > 0)
+            while (hats.Count > 0 && scarves.Count > 0)
             {
-                if (hats[hats.Count - 1] > scarfs[0])
+                if (hats[hats.Count - 1] > scarves[0])
                 {
-                    int sum = hats[hats.Count - 1] + scarfs[0];
+                    int sum = hats[hats.Count - 1] + scarves[0];
                     sets.Add(sum);
                     hats.RemoveAt(hats.Count - 1);
-                    scarfs.RemoveAt(0);
+                    scarves.RemoveAt(0);
                 }
-                else if (hats[hats.Count - 1] < scarfs[0])
+                else if (hats[hats.Count - 1] < scarves[0])
                 {
                     hats.RemoveAt(hats.Count - 1);
                 }
                 else
                 {
-                    scarfs.RemoveAt(0);
+                    scarves.RemoveAt(0);
                     hats[hats.Count - 1]++;
                 }
             }
